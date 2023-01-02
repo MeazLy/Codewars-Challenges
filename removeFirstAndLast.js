984f1698e6ceeda7b09d795efbe256d5d0365f53
Morgan <mewanchuk@live.com>
2023-01-02 12:44:28 -0700

    new challenge, remove first and last char

---
+++ removeFirstAndLast.js
@@ -0,0 +1,13 @@
+// Given a string remove the first and last characters
+
+// Given "pilsbury" return "ilsbur"
+// Given "doughboy" return "oughbo"
+
+function removeFirstAndLast(str){
+    return str.slice(1, -1);
+};
+
+removeFirstAndLast("pilsbury");
+removeFirstAndLast("doughboy")
+
+
