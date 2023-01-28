5279127a75ac996e9852db2e8bd0bc0bb21a670e
Morgan <mewanchuk@live.com>
2023-01-28 15:38:33 -0700

    new challenge, determine if string has unique char

---
+++ onlyUniqueChars.js
@@ -0,0 +1,12 @@
+// Given a string, return true if the string contains only unique characters and false if not. String can contain symbols as well.
+// Characters are case sensitive, "d" is a different character than "D".
+// Example: Given "hgfijk" return true
+
+
+function hasUniqueChars(str){
+    let unique = new Set(str)
+    
+    return unique.size === str.split("").length ? true : false
+};
+
+hasUniqueChars("qwerty") // true
